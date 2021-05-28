2c67ab91223477c0d292f2799b6d5d046664dfc0
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-05-28 12:58:23 +0200

    changed index-routes.js to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
-import routes from './routes/index-routes'
+import routes from './routes/index.js'
 
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use(express.urlencoded({ extended: true }))
 
 app.use(routes)
 
